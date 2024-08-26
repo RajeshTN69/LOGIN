7ae050433b1ba767daeb6ba18c0b9c5e9305abec
RajeshTN69 <157153257+RajeshTN69@users.noreply.github.com>
2024-08-26 10:20:54 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,8 @@
+<?php
+$name =$_POST['name'];
+$class =$_POST['class'];
+$age =$_POST['age'];
+$date =$_POST['date'];
+$gender =$_POST['gender'];
+$father=$_POST['father'];
+$mother =$_POST['mother'];
